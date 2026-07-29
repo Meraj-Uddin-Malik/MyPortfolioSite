ac8578653446e565bf2d47815b878bbb18ec6007
Meraj Uddin Malik <141-2021@lms.indus.edu.pk>
2026-07-30 01:22:22 +0500

    Fix: restore missing imports in work/page.jsx

--- app/work/page.jsx
+++ app/work/page.jsx
@@ -1,3 +1,22 @@
+"use client";
+
+import { motion } from "framer-motion";
+import React, { useState } from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import { BsArrowUpRight, BsGithub } from "react-icons/bs";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@radix-ui/react-tooltip";
+import Link from "next/link";
+
+//component import
+import WorkSliderBtns from "@/components/WorkSliderBtns";
+import Image from "next/image";
+
 const projects = [
   {
     num: "01",
